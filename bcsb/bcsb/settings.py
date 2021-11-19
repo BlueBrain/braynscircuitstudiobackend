df2a8586fd53509d6e545daa1f91d1c5a470fedd
Marek Naskret <marek.naskret@epfl.ch>
2021-11-19 11:49:07 +0100

    Update settings and requirements

--- bcsb/bcsb/settings.py
+++ bcsb/bcsb/settings.py
@@ -1,9 +1,10 @@
+from os import getenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = "django-insecure-8n)#3#1bto-d-lcaw5i#xvpz3%r(8a_6vsqp1*&tfov4zbd(k_"
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = getenv("DJANGO_SECRET_KEY")
+DEBUG = bool(int(getenv("DJANGO_DEBUG", "0")))
+ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -46,8 +47,15 @@ WSGI_APPLICATION = "bcsb.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": getenv("BACKEND_DB_HOST"),
+        "PORT": getenv("BACKEND_DB_PORT"),
+        "NAME": getenv("BACKEND_DB_NAME"),
+        "USER": getenv("BACKEND_DB_USER"),
+        "PASSWORD": getenv("BACKEND_DB_PASSWORD"),
+        "TEST": {
+            "NAME": getenv("BACKEND_TEST_DB_NAME", "bcsb_test"),
+        },
     }
 }
 
@@ -79,3 +87,15 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ASGI_APPLICATION = "bcsb.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [
+                (getenv("COMPOSE_REDIS_HOST"), getenv("COMPOSE_REDIS_PORT")),
+            ],
+        },
+    },
+}
