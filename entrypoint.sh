0010c96bc1ff7b110c26c4071288767aac0bf4d4
Marek Naskret <marek.naskret@epfl.ch>
2021-12-02 11:35:07 +0100

    Update entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export PYTHONPATH="${PYTHONPATH}:/usr/src/app/"
+export PYTHONPATH="${PYTHONPATH}:/usr/src/braynscircuitstudiobackend/"
 
 echo "Environment = ${ENVIRONMENT_MODE}"
 
@@ -13,8 +13,11 @@ if [ "$ENVIRONMENT_MODE" = "production" ]; then
 fi
 
 if [ "$ENVIRONMENT_MODE" = "development" ]; then
-  # Development
-  exec python manage.py runserver ${COMPOSE_BACKEND_HOST}:${COMPOSE_BACKEND_PORT}
+  exec python /usr/src/braynscircuitstudiobackend/manage.py runserver ${COMPOSE_BACKEND_HOST:-0.0.0.0}:${COMPOSE_BACKEND_PORT:-8000}
 fi
+
+if [ -z "$ENVIRONMENT_MODE"]; then
+  echo "ENVIRONMENT_MODE variable is not set. Please specify either 'development' or 'production'."
+fi;
 
 exec "$@"
