47bd62aced51aea1b4c34c6ea836075a140ca3cc
Marek Naskret <marek.naskret@epfl.ch>
2022-02-17 13:14:32 +0100

    Remove JobService (merge into UnicoreService)

--- braynscircuitstudiobackend/bcsb/unicore/tests/test_unicore_service.py
+++ braynscircuitstudiobackend/bcsb/unicore/tests/test_unicore_service.py
@@ -1,11 +1,80 @@
 from asyncio import Future
+from datetime import datetime
 from http import HTTPStatus
 from unittest.mock import AsyncMock, MagicMock
+from uuid import UUID
 
 import pytest
 from furl import furl
+from pytest_mock import MockerFixture
+from pytz import UTC
 
-from bcsb.unicore.unicore_service import UnicoreService, ClientResponse
+from bcsb.unicore.unicore_service import UnicoreService, ClientResponse, UnicoreJobStatus
+
+MOCK_JOB_LIST_RESPONSE = {
+    "_links": {
+        "self": {"href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs?offset=0&num=200"}
+    },
+    "jobs": [
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/e57c4fe1-f9dc-4b67-830f-375594b7d73c",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/31fc04e5-4d78-4d66-b377-0b348262866a",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/31a580c5-7d48-41d6-bcd2-3cc3dcef330b",
+    ],
+}
+
+
+MOCK_JOB_RESPONSE = {
+    "owner": "CN=naskret, O=Ecole polytechnique federale de Lausanne (EPFL), L=Lausanne, ST=Vaud, C=CH",
+    "submissionPreferences": {"UC_OAUTH_BEARER_TOKEN": ["ey...JQ"]},
+    "log": [
+        "Mon Jan 24 16:07:26 CET 2022: Created with ID fb82eb95-04eb-4fca-9b7e-2650c499ca45",
+        "Mon Jan 24 16:07:26 CET 2022: Created with type 'JSDL'",
+        "Mon Jan 24 16:07:26 CET 2022: Client: Name: CN=naskret,O=Ecole polytechnique federale de Lausanne (EPFL),L=Lausanne,ST=Vaud,C=CH\nXlogin: uid: [naskret], gids: [addingOSgroups: true]\nRole: user: role from attribute source\nSecurity tokens: User name: CN=naskret,O=Ecole polytechnique federale de Lausanne (EPFL),L=Lausanne,ST=Vaud,C=CH\nDelegation to consignor status: true, core delegation status: false\nMessage signature status: UNCHECKED\nClient's original IP: 128.179.254.207",
+        "Mon Jan 24 16:07:27 CET 2022: Using default execution environment.",
+        "Mon Jan 24 16:07:27 CET 2022: No staging in needed.",
+        "Mon Jan 24 16:07:27 CET 2022: Status set to READY.",
+        "Mon Jan 24 16:07:27 CET 2022: Status set to PENDING.",
+        "Mon Jan 24 16:07:27 CET 2022: No application to execute, changing action status to POSTPROCESSING",
+        "Mon Jan 24 16:07:27 CET 2022: Status set to DONE.",
+        "Mon Jan 24 16:07:27 CET 2022: Result: Success.",
+        "Mon Jan 24 16:07:27 CET 2022: Total: 0.28 sec., Stage-in: 0.00 sec., Stage-out: 0.00 sec., Datamovement: 1 %",
+    ],
+    "_links": {
+        "action:start": {
+            "description": "Start",
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/start",
+        },
+        "action:restart": {
+            "description": "Restart",
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/restart",
+        },
+        "workingDirectory": {
+            "description": "Working directory",
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/storages/fb82eb95-04eb-4fca-9b7e-2650c499ca45-uspace",
+        },
+        "self": {
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45"
+        },
+        "action:abort": {
+            "description": "Abort",
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/abort",
+        },
+        "parentTSS": {
+            "description": "Parent TSS",
+            "href": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/sites/800e4953-0c70-4e86-a331-c8515f463df1",
+        },
+    },
+    "acl": [],
+    "submissionTime": "2022-01-24T16:07:27+0100",
+    "statusMessage": "",
+    "tags": [],
+    "currentTime": "2022-01-24T16:07:48+0100",
+    "resourceStatus": "READY",
+    "terminationTime": "2022-02-23T16:07:26+0100",
+    "name": "UNICORE_Job",
+    "queue": "N/A",
+    "status": "SUCCESSFUL",
+}
 
 
 @pytest.mark.asyncio
@@ -91,3 +160,170 @@ async def test_http_request_unicore(mocker, unicore_service: UnicoreService):
 
     await unicore_service.http_post_unicore("jobs", {})
     mock_http_request.assert_called_with("post", "jobs", {})
+
+
+@pytest.mark.asyncio
+async def test_get_jobs(mocker, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.status = HTTPStatus.OK
+    mock_response.json.return_value = MOCK_JOB_LIST_RESPONSE
+    mock_get: MagicMock = mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.http_get_unicore",
+        return_value=mock_response,
+    )
+    jobs = await unicore_service.get_jobs()
+    assert len(jobs) == 3
+    assert all(isinstance(job_id, UUID) for job_id in jobs)
+
+    job_id: UUID = jobs[2]
+    assert job_id == UUID("31a580c5-7d48-41d6-bcd2-3cc3dcef330b")
+
+
+@pytest.mark.asyncio
+async def test_create_job(mocker, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.status = HTTPStatus.CREATED
+    mock_response.json.return_value = ""
+    mock_response.headers = {
+        "Location": "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/31a580c5-7d48-41d6-bcd2-3cc3dcef330b"
+    }
+
+    mock_post = mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.http_post_unicore",
+        return_value=mock_response,
+    )
+
+    job_id = await unicore_service.create_job(
+        project="proj3",
+        name="My Visualization",
+        memory="128G",
+        runtime="4h",
+    )
+
+    mock_post.assert_called()
+
+    assert job_id == UUID("31a580c5-7d48-41d6-bcd2-3cc3dcef330b")
+
+
+@pytest.mark.asyncio
+async def test_get_job_status(mocker, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.status = HTTPStatus.OK
+    mock_response.json.return_value = MOCK_JOB_RESPONSE
+    mock_get: MagicMock = mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.http_get_unicore",
+        return_value=mock_response,
+    )
+
+    job_status = await unicore_service.get_job_status(UUID("fb82eb95-04eb-4fca-9b7e-2650c499ca45"))
+    mock_get.assert_called_once()
+
+    assert isinstance(job_status, UnicoreJobStatus)
+
+    assert job_status.status == "SUCCESSFUL"
+    assert job_status.is_successful
+    assert not job_status.is_queued
+    assert not job_status.is_running
+
+    # Original time was +0100 but we can check it using UTC reference by subtracting 1 hour
+    assert job_status.current_time == datetime(2022, 1, 24, 15, 7, 48, tzinfo=UTC)
+    assert job_status.submission_time == datetime(2022, 1, 24, 15, 7, 27, tzinfo=UTC)
+
+
+@pytest.mark.asyncio
+def test_get_unicore_file_url(unicore_service: UnicoreService):
+    assert (
+        unicore_service.get_unicore_file_url("fb82eb95-04eb-4fca-9b7e-2650c499ca45", "hostname").url
+        == "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/storages/fb82eb95-04eb-4fca-9b7e-2650c499ca45-uspace/files/hostname"
+    )
+
+
+@pytest.mark.asyncio
+async def test_download_file(mocker: MockerFixture, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.__aenter__.return_value.status = HTTPStatus.OK
+    mock_response.__aenter__.return_value.content.read.return_value = b"Hello there"
+
+    mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.make_unicore_http_request",
+        return_value=mock_response,
+    )
+
+    mock_get_response = AsyncMock(ClientResponse)
+    mock_get_response.status = HTTPStatus.OK
+    mock_get_response.json.return_value = {}
+    mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.http_get_unicore",
+        return_value=mock_get_response,
+    )
+
+    job_id = UUID("fb82eb95-04eb-4fca-9b7e-2650c499ca45")
+    async with await unicore_service.download_file(job_id, "my_file.txt") as job_file_response:
+        assert job_file_response.status == HTTPStatus.OK
+        file_content = await job_file_response.content.read()
+
+    assert file_content == b"Hello there"
+
+
+@pytest.mark.asyncio
+async def test_upload_text_file(mocker: MockerFixture, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.status = HTTPStatus.NO_CONTENT
+    mock_http_request = mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.make_unicore_http_request",
+        return_value=mock_response,
+    )
+
+    job_id = UUID("fb82eb95-04eb-4fca-9b7e-2650c499ca45")
+    response = await unicore_service.upload_text_file(job_id, "my_file.txt", "Hello")
+    extra_headers = {
+        "Accept": "application/octet-stream",
+        "Content-Type": "text/plain",
+    }
+
+    mock_http_request.assert_called_once_with(
+        "put",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/storages/fb82eb95-04eb-4fca-9b7e-2650c499ca45-uspace/files/my_file.txt",
+        extra_headers=extra_headers,
+    )
+
+    assert response.status == HTTPStatus.NO_CONTENT
+
+
+@pytest.mark.asyncio
+async def test_start_job(mocker: MockerFixture, unicore_service: UnicoreService):
+    mock_response = AsyncMock(ClientResponse)
+    mock_response.status = HTTPStatus.NO_CONTENT
+    mock_http_request = mocker.patch(
+        "bcsb.unicore.unicore_service.UnicoreService.make_unicore_http_request",
+        return_value=mock_response,
+    )
+
+    job_id = UUID("fb82eb95-04eb-4fca-9b7e-2650c499ca45")
+    await unicore_service.start_job(job_id)
+
+    mock_http_request.assert_called_once_with(
+        "post",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/start",
+        None,
+    )
+
+    mock_http_request.reset_mock()
+
+    await unicore_service.restart_job(job_id)
+
+    mock_http_request.assert_called_once_with(
+        "post",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/restart",
+        None,
+    )
+    mock_http_request.reset_mock()
+
+    await unicore_service.abort_job(job_id)
+
+    mock_http_request.assert_called_once_with(
+        "post",
+        "https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/fb82eb95-04eb-4fca-9b7e-2650c499ca45/actions/abort",
+        None,
+    )
+    mock_http_request.reset_mock()
--- braynscircuitstudiobackend/bcsb/unicore/unicore_service.py
+++ braynscircuitstudiobackend/bcsb/unicore/unicore_service.py
@@ -1,6 +1,32 @@
-from aiohttp import ClientResponse, ClientSession
+import logging
+from datetime import datetime
+from http import HTTPStatus
+from typing import List
+from uuid import UUID
+
+from aiohttp import ClientResponse
+from aiohttp import ClientSession
 from django.conf import settings
 from furl import furl
+from pydash import get
+
+from bcsb.unicore.schemas import (
+    CreateJobSchema,
+    dump_schema,
+    JobStatusResponseSchema,
+    load_schema,
+    JobListResponseSchema,
+)
+from utils.strings import equals_ignoring_case
+from utils.uuid import extract_uuid_from_text
+
+START = "start"
+ABORT = "abort"
+RESTART = "restart"
+JOB_ACTIONS = {START, ABORT, RESTART}
+
+
+logger = logging.getLogger(__name__)
 
 
 class UnicoreService:
@@ -61,3 +87,144 @@ class UnicoreService:
             endpoint,
             payload,
         )
+
+    def _get_job_id_from_create_job_response(self, response: ClientResponse):
+        # https://bbpunicore.epfl.ch:8080/BB5-CSCS/rest/core/jobs/b7c5c49a-078e-4b2a-ac4d-0def93b70635
+        location_url = response.headers["Location"]
+        job_uuid = extract_uuid_from_text(location_url)
+        assert isinstance(job_uuid, UUID)
+        return job_uuid
+
+    async def get_jobs(self) -> List[UUID]:
+        response = await self.http_get_unicore("/jobs")
+        assert response.status == HTTPStatus.OK
+        schema = load_schema(JobListResponseSchema, await response.json())
+        return [extract_uuid_from_text(job_url) for job_url in get(schema, "jobs", [])]
+
+    async def create_job(
+        self,
+        project: str,
+        name: str,
+        have_clients_stage_in: bool = True,
+        queue: str = "prod",
+        nodes: int = 1,
+        cpus_per_node: int = 72,
+        runtime: str = "8h",
+        node_constraints: str = "cpu",
+        memory: str = "128G",
+        tags: List[str] = None,
+        exclusive: bool = True,
+    ) -> UUID:
+        tags = tags or ["visualization"]
+        payload = dump_schema(
+            CreateJobSchema,
+            {
+                "project": project,
+                "name": name,
+                "have_client_stage_in": have_clients_stage_in,
+                "tags": tags,
+                "resources": {
+                    "queue": queue,
+                    "nodes": nodes,
+                    "cpus_per_node": cpus_per_node,
+                    "runtime": runtime,
+                    "node_constraints": node_constraints,
+                    "memory": memory,
+                    "exclusive": exclusive,
+                },
+            },
+        )
+        response = await self.http_post_unicore("/jobs", payload=payload)
+        assert (
+            response.status == HTTPStatus.CREATED
+        ), f"Unexpected response status: {response.status}"
+        job_id = self._get_job_id_from_create_job_response(response)
+        return job_id
+
+    async def get_job_status(self, job_id: UUID):
+        response = await self.http_get_unicore(f"jobs/{job_id}")
+        assert response.status == HTTPStatus.OK, f"Unexpected response status: {response.status}"
+        logger.debug("Job status")
+        return UnicoreJobStatus(await response.json())
+
+    def get_unicore_file_url(self, job_id: UUID, file_path: str) -> furl:
+        url = self.get_unicore_furl()
+        url /= f"/storages/{job_id}-uspace/files/"
+        url /= file_path
+        return url
+
+    async def download_file(self, job_id: UUID, file_path: str):
+        file_url = self.get_unicore_file_url(job_id, file_path)
+        download_file_headers = {
+            "Accept": "application/octet-stream",
+        }
+        response = await self.make_unicore_http_request(
+            "get", file_url.url, extra_headers=download_file_headers
+        )
+        return response
+
+    async def upload_text_file(self, job_id: UUID, file_path: str, text_content: str):
+        file_url = self.get_unicore_file_url(job_id, file_path)
+        upload_file_headers = {
+            "Accept": "application/octet-stream",
+            "Content-Type": "text/plain",
+        }
+        response = await self.make_unicore_http_request(
+            "put", file_url.url, extra_headers=upload_file_headers
+        )
+        assert (
+            response.status == HTTPStatus.NO_CONTENT
+        ), f"Unexpected response status: {response.status}"
+        return response
+
+    def _get_job_action_url(self, job_id: UUID, action: str):
+        url = self.get_unicore_furl()
+        assert action in JOB_ACTIONS
+        url /= f"jobs/{job_id}/actions/{action}"
+        return url
+
+    async def _run_job_action(self, job_id: UUID, action: str):
+        return await self.http_post_unicore(self._get_job_action_url(job_id, action).url)
+
+    async def start_job(self, job_id: UUID):
+        return await self._run_job_action(job_id, START)
+
+    async def abort_job(self, job_id: UUID):
+        return await self._run_job_action(job_id, ABORT)
+
+    async def restart_job(self, job_id: UUID):
+        return await self._run_job_action(job_id, RESTART)
+
+
+class UnicoreJobStatus:
+    QUEUED = "QUEUED"
+    RUNNING = "RUNNING"
+    SUCCESSFUL = "SUCCESSFUL"
+    FAILED = "FAILED"
+
+    def __init__(self, raw_data: dict):
+        self._response_schema = load_schema(JobStatusResponseSchema, raw_data)
+
+    @property
+    def status(self):
+        return get(self._response_schema, "status")
+
+    @property
+    def is_queued(self):
+        return equals_ignoring_case(self.status, self.QUEUED)
+
+    @property
+    def is_successful(self):
+        return equals_ignoring_case(self.status, self.SUCCESSFUL)
+
+    @property
+    def is_running(self):
+        return equals_ignoring_case(self.status, self.RUNNING)
+
+    @property
+    def current_time(self) -> datetime:
+        return get(self._response_schema, "current_time")
+
+    @property
+    def submission_time(self) -> datetime:
+        return get(self._response_schema, "submission_time")
