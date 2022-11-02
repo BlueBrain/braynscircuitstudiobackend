4b6d53a407858b2569535cae6d650f5d903fc28e
Marek Naskret <marek.naskret@epfl.ch>
2022-11-02 08:43:02 +0100

    Fix serializer data in start-new-session method

--- apps/bcsb/brayns/serializers.py
+++ apps/bcsb/brayns/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
 
 from bcsb.allocations.serializers import AllocationSerializer
-from bcsb.sessions.serializers import SessionListItemSerializer
+from bcsb.sessions.models import Session
 
 
 class StartBraynsRequestSerializer(serializers.Serializer):
     project = serializers.CharField(default="proj3")
 
 
+class NewSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Session
+        fields = [
+            "id",
+            "session_uid",
+            "created_at",
+            "ready_at",
+        ]
+
+
 class StartBraynsResponseSerializer(serializers.Serializer):
-    session = SessionListItemSerializer()
+    session = NewSessionSerializer()
     allocation = AllocationSerializer()
