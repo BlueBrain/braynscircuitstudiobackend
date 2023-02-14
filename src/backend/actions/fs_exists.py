6573319d5413a50054135e3d44e8f1d685bf6297
Marek Naskret <marek.naskret@epfl.ch>
2023-02-14 15:29:34 +0100

    Implement fs-exists action

--- src/backend/actions/fs_exists.py
+++ src/backend/actions/fs_exists.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from marshmallow import Schema, fields
 import os
 from backend.jsonrpc.actions import Action
@@ -7,15 +9,36 @@ class FsExistsRequestSchema(Schema):
     path = fields.String(required=True)
 
 
+class FsExistsResponseSchema(Schema):
+    exists = fields.Boolean()
+    type = fields.String(
+        required=False,
+        allow_none=True,
+    )
+
+
+@dataclass
+class FilesystemObject:
+    type: str = None
+
+    @property
+    def exists(self) -> bool:
+        return self.type is not None
+
+
 class FsExists(Action):
     request_schema = FsExistsRequestSchema
+    response_schema = FsExistsResponseSchema
 
     async def run(self):
         path = self.request.params["path"]
         absolute_path = os.path.abspath(path)
 
+        filesystem_object = FilesystemObject()
+
         if os.path.isdir(absolute_path):
-            return {"type": "directory"}
+            filesystem_object.type = "directory"
         if os.path.isfile(absolute_path):
-            return {"type": "file"}
-        return {"type": "none"}
+            filesystem_object.type = "file"
+
+        return filesystem_object
