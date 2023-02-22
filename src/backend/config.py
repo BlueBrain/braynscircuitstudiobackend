af95eff6bb4a779890703b47cb13b2e945cca838
Marek Naskret <marek.naskret@epfl.ch>
2023-02-22 14:43:06 +0100

    USE_TLS by default

--- src/backend/config.py
+++ src/backend/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 APP_HOST = getenv("APP_HOST", "0.0.0.0")
 APP_PORT = int(getenv("APP_PORT", 8000))
-USE_TLS = bool(int(getenv("USE_TLS", "0")))
+USE_TLS = bool(int(getenv("USE_TLS", "1")))
 LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
 BASE_DIR = Path(__file__).resolve().parent
 IS_SENTRY_ENABLED = bool(int(getenv("IS_SENTRY_ENABLED", "1")))
