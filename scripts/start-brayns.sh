4b8c4059dd3ac78924ed346a8f024f0cb1fbc5e7
Marek Naskret <marek.naskret@epfl.ch>
2023-03-07 12:07:39 +0100

    Update brayns script

--- scripts/start-brayns.sh
+++ scripts/start-brayns.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
 
+export BRAYNS_PATH=/gpfs/bbp.cscs.ch/project/proj3/software/BraynsCircuitStudio/ab2704b/braynsService
 export BCS_DIR=/gpfs/bbp.cscs.ch/project/proj3/software/BraynsCircuitStudio/
 export BRAYNS_PORT=5000
 export LOG_LEVEL=DEBUG
 
 echo "Starting Brayns..."
-${BCS_DIR}brayns-{{BRAYNS_VERSION}} \
+${BCS_DIR}${BRAYNS_PATH} \
   --uri 0.0.0.0:$BRAYNS_PORT \
   --log-level debug \
   --plugin braynsCircuitExplorer \
