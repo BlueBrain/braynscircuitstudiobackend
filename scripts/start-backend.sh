85f27cb5a6269068ce01722392a21cc8480963a3
Marek Naskret <marek.naskret@epfl.ch>
2023-03-07 14:31:11 +0100

    Update scripts

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -3,9 +3,10 @@
 export BACKEND_DIR=/gpfs/bbp.cscs.ch/project/proj3/software/BraynsCircuitStudio/backend/
 export BACKEND_PORT=8000
 export LOG_LEVEL=DEBUG
+export HOSTNAME=$(hostname -f)
 
 echo "Activating Python virtual environment..."
 source ${BACKEND_DIR}venv/bin/activate
 echo "Starting backend..."
 
-${BACKEND_DIR}src/main.py --port=$BACKEND_PORT >backend-$1.log 2>&1
+python ${BACKEND_DIR}src/main.py --port=$BACKEND_PORT >backend-${HOSTNAME}.log 2>&1
--- scripts/start-brayns.sh
+++ scripts/start-brayns.sh
@@ -3,6 +3,7 @@
 export BRAYNS_PATH=/gpfs/bbp.cscs.ch/project/proj3/software/BraynsCircuitStudio/ab2704b/braynsService
 export BRAYNS_PORT=5000
 export LOG_LEVEL=DEBUG
+export HOSTNAME=$(hostname -f)
 
 echo "Starting Brayns on port ${BRAYNS_PORT}..."
 
@@ -10,4 +11,4 @@ ${BRAYNS_PATH} \
   --uri 0.0.0.0:$BRAYNS_PORT \
   --log-level debug \
   --plugin braynsCircuitExplorer \
-  --plugin braynsAtlasExplorer >brayns-$1.log 2>&1
+  --plugin braynsAtlasExplorer >brayns-${HOSTNAME}.log 2>&1
