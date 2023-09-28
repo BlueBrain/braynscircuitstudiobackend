43e81b86badcfe2bee7f6e76f29f92646974fe6f
Adrien Christian Fleury <adrien.fleury@epfl.ch>
2023-09-28 13:47:44 +0000

    BCSB-41 Remove async wrapper.

--- bcsb/entrypoint.py
+++ bcsb/entrypoint.py
@@ -1,8 +1,14 @@
+import asyncio
+
 from .factory import create_service
-from .settings import parse_argv
+from .settings import Settings, parse_argv
+
+
+async def run_service(settings: Settings) -> None:
+    service = await create_service(settings)
+    await service.run()
 
 
 def run() -> None:
     settings = parse_argv()
-    service = create_service(settings)
-    service.run()
+    asyncio.run(run_service(settings))
